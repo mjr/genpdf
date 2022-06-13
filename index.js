00eb97e21c2914f7eda537aaeed388c53f644439
Manaia Junior <manaiajr.23@gmail.com>
2022-06-13 01:49:18 -0300

    Update launch setup

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ app.get('/pdf', async (req, res) => {
   }
 
   try {
-    const browser = await chromium.puppeteer.launch()
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    })
     const page = await browser.newPage()
     await page.goto(url, { waitUntil: 'networkidle0' })
     const pdf = await page.pdf({ printBackground: true, format: 'A4' })
